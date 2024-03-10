94b5f55b108065b0161501537e7e230d518b42d6
Iryna-Pavlyk <145845654+Iryna-Pavlyk@users.noreply.github.com>
2024-03-10 11:12:27 +0200

    post data

--- src/api/index.js
+++ src/api/index.js
@@ -1,2 +1,16 @@
 const BASE_URL = "http://localhost:3000/contacts/";
 
+export function saveData(data) {
+  return fetch(BASE_URL, {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((response) => {
+    if (!response.ok) {
+      throw new Error(response.statusText || response.status);
+    }
+    return response.json();
+  });
+}
--- src/main.js
+++ src/main.js
@@ -1,2 +1,26 @@
+import { formEl } from "./refs";
+import { saveData } from "./api";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/style.css";
+
+formEl.addEventListener("submit", postData);
+
+async function postData(event) {
+  try {
+    event.preventDefault();
+    const data = Object.fromEntries(new FormData(event.currentTarget));
+    const resonse = await saveData(data);
+  } catch (error) {
+    console.log(error.message);
+  }
+
+  //   const { name, number, email } = event.currentTarget.elements;
+  //   const nameValue = name.value.trim();
+  //   const numberValue = number.value.trim();
+  //   const emailValue = email.value.trim();
+  //   console.log(nameValue, numberValue, emailValue);
+}
+
+// const arr = Object.entries({ name: "Poly", number: 0 });
+// console.log(arr);
+// console.log(Object.fromEntries(arr));
