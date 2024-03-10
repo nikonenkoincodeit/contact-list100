6c3abe504ea20903d1c6d4b54d365d41b720ea37
nikonenkoincodeit <75611895+nikonenkoincodeit@users.noreply.github.com>
2024-03-10 10:08:07 +0200

    start

--- src/api/index.js
+++ src/api/index.js
@@ -1,26 +1,2 @@
 const BASE_URL = "http://localhost:3000/contacts/";
 
-export function saveData(bodyData) {
-  const data = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(bodyData),
-  };
-  return fetch(BASE_URL, data).then((result) => {
-    if (!result.ok) {
-      throw new Error(result.statusText || result.status);
-    }
-    return result.json();
-  });
-}
-
-export function dataClient() {
-  return fetch(BASE_URL).then((result) => {
-    if (!result.ok) {
-      throw new Error(result.statusText || result.status);
-    }
-    return result.json();
-  });
-}
